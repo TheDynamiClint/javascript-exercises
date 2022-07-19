80a882a2f6853e133e3908e66505c6acdee57209
TheDynamiClint <69874342+TheDynamiClint@users.noreply.github.com>
2022-07-19 08:36:52 -1000

    Update reverseString.js with passing code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,26 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    let drow = '';
+
+    if  (word == '') {
+
+        drow = '';
+
+    } else {
+
+        wordArray = [];
+
+        for (i = 0; i < word.length; i++) {
+            let inputChar = word[i];
+            wordArray.push(inputChar);
+        }
+
+        wordArray = wordArray.reverse();
+        drow = wordArray.join('');
+
+    }
+
+    return drow;
 };
 
 // Do not edit below this line
