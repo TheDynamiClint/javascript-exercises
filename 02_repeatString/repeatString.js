8300206a4b04305448fd8ec0862f67b27dae70ee
TheDynamiClint <69874342+TheDynamiClint@users.noreply.github.com>
2022-07-19 07:53:32 -1000

    Update repeatString.js with passing code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,27 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let newString = [];
+    let returnPhrase = '';
+
+    if (num >= 0 && string != '') {
+
+            for(let i = 0; i < num; i++) {
+                    newString.push(string);
+                }
+
+                returnPhrase = newString.join('');
+
+    } else if (num < 0) {
+
+        returnPhrase = 'ERROR';
+
+    } else {
+
+        returnPhrase = '';
+
+    }
+
+    return returnPhrase;
 };
 
 // Do not edit below this line
