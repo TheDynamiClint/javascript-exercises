c9f9c2ff6a9ad03dbd121b71b0eea4eb22299e84
TheDynamiClint <69874342+TheDynamiClint@users.noreply.github.com>
2022-07-20 00:39:36 -1000

    Update removeFromArray.js with successfully tested code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(args) {
+    
+    //grab the first index value, which should be the input array
+    let valuesArray = [];
+    valuesArray = arguments[0];
 
+    //place remaining index values into a second array
+    let charsToRemove = [];
+    for (let i = 1; i < arguments.length; i++) {
+        charsToRemove[i-1] = arguments[i];
+    }
+    
+    //loop through charsToRemove and remove those values from args array
+    let filtered = [];
+        
+    for (let i = 0; i < charsToRemove.length; i++) {
+        valuesArray = valuesArray.filter(function(value, charToRemove) {
+        return value !== charsToRemove[i];
+        });
+        }
+    
+    return valuesArray;
 };
 
 // Do not edit below this line
